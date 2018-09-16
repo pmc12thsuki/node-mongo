10487263aafd7b7ce8d89e96c11ccaa034754214
suki wang <pmc12thsuki@gmail.com>
2018-09-16 23:50:32 +0800

    Add GET /todos/:id and test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser'); //take json into object, attach on request object
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -18,6 +19,29 @@ app.post('/todos',(req, res)=>{
         res.send(result);
     },e =>{
         res.status(400).send(e);
+    })
+})
+
+app.get('/todos',(req, res)=>{
+    Todo.find().then(todos=>{
+        res.send({todos}); // send result back in object
+    },e=>{
+        res.status(400).send(e);
+    })
+})
+
+app.get('/todos/:id', (req, res)=>{
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){ // valid id
+        return res.status(404).send();
+    }
+    Todo.findById(id).then(todo=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    },error=>{
+        res.status(400).send();
     })
 })
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,24 @@
 'use strict';
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-beforeEach((done)=>{ //setup environment before test , eg: clear our db
-    Todo.remove({}).then(()=> done()); // remove all documents in Todo collection
+const todos =[{
+    _id: new ObjectID(),
+    text: 'first todo'
+},{
+    _id: new ObjectID(),
+    text: 'second todo'
+}];
+
+
+beforeEach((done)=>{ //setup environment (default db) before test 
+    Todo.remove({}).then(()=> { // clear our db, remove all documents in Todo collection
+        return Todo.insertMany(todos); // add default todos
+    }).then(()=>done()) 
 })
 
 describe('Post /todos',()=>{
@@ -24,15 +36,13 @@ describe('Post /todos',()=>{
             if(err) return done(err);
 
             //then check db operation is correct
-            Todo.find().then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text); //check database correct
                 done();
             }).catch(e=>done(e));
         })
     })
-
-
 
 
     it('should not create todo with invalid body data',(done)=>{
@@ -45,7 +55,7 @@ describe('Post /todos',()=>{
             if (err) return done(err);
 
             Todo.find().then(todos=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch(e=>{
                 done(e);
@@ -53,3 +63,44 @@ describe('Post /todos',()=>{
         })
     })
 });
+
+
+describe('GET /todos',()=>{
+    it('should get all todos',(done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect(res=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    })
+})
+
+describe('GET /todos/:id', ()=>{
+    it('should return todo doc', done=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect(res=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    })
+
+    it('should return 404 if todo not found', (done)=>{
+        const id = new ObjectID();
+        request(app)
+        .get(`/todos/${id.toHexString()}`)
+        .expect(404)
+        .end(done);
+    })
+
+    it('should return 404 if invalid', (done)=>{
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
+    })
+
+})
